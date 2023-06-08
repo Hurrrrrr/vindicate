5f3150125c81be223490cee8583714dd2ef35db3
jp <jonahpalmer@gmail.com>
2023-06-08 18:29:16 -0400

    begin separating main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import random
 import sqlite3
 
 class Wine:
-    def __init__(self, scope, style, label_color, country, region,        ## label attributes
-    appellation, vintage,
+    def __init__(self, id, scope, style, label_color, country, region,        ## label attributes
+    appellation, grapes, vintage,
     clarity, appearance_red, appearance_green, appearance_blue, appearance_other,                  ## appearance
     condition, nose_intensity, development,                                 ## nose
     petillance, sweetness, acidity, alcohol,                                ## structure
@@ -11,6 +11,8 @@ class Wine:
     fruit_color, fruit_family,                                              ## palate
     fruit_condition, fruit_subcondition, floral, herbaceous, herbal,
     earth_organic, earth_inorganic, grape_spice, oak, aroma_other):
+        self.id = id
+
         ##  0-3; 0 = narrow, 1 = medium, 2 = wide, 3 = very wide
         self.scope = scope
 
@@ -21,9 +23,6 @@ class Wine:
         ##  display = graphic
         self.label_color = label_color
 
-        ##  list
-        # self.grapes = grapes
-
         ##  string
         self.country = country
 
@@ -32,6 +31,9 @@ class Wine:
 
         ##  string
         self.appellation = appellation
+
+        ##  list
+        self.grapes = grapes
 
         ##  int
         self.vintage = vintage
@@ -81,7 +83,7 @@ class Wine:
         ##  display = radar chart
         self.body = body
 
-        ##  0-255; 0-50 = low, 51-101 = med-, 102-152 = med, 153-203 = med+, 204-255 = high
+        ##  0-255; 0-29 = none, 30-50 = low, 51-101 = med-, 102-152 = med, 153-203 = med+, 204-255 = high
         ##  display = radar chart
         self.tannin_or_bitterness = tannin_or_bitterness
 
@@ -97,7 +99,7 @@ class Wine:
         ##  0-255; white: 0-27 = tart citrus, 28-55 = tart pome, 56-83 = sweet citrus,
         ##  84-111 = tart stone, 112-139 = tart tropical, 140-167 = sweet pome,
         ##  168-195 = sweet stone, 196-223 = sweet tropical, 224-255 = melon
-        ##  red: vegetal, red, blue, black
+        ##  red: 0-63 = vegetal, 64-127 = tart berry, 128-191 = sweet stone, 192-255 = rich fleshy
         ##  display = text
         self.fruit_family = fruit_family
 
@@ -147,10 +149,365 @@ class Wine:
         ##  display = text
         self.aroma_other = aroma_other
 
+    def __repr__(self):
+        class_name = type(self).__name__
+        return f"Wine({', '.join(f'{k}={v}' for k, v in vars(self).items())})"
+    
+    def get_clarity(self):
+        return self.clarity
+    
+    # finish this once this has a graphical display
+    def get_appearance(self):
+        return f"{self.appearance_red},{self.appearance_green},{self.appearance_blue}"
+    
+    def get_appearance_other(self):
+        return self.appearance_other
+    
+    def get_condition(self):
+        return self.condition
+    
+    def get_nose_intensity(self):
+        if self.nose_intensity >= 204:
+            return "High"
+        elif self.nose_intensity >= 153:
+            return "Medium-Plus"
+        elif self.nose_intensity >= 102:
+            return "Medium"
+        elif self.nose_intensity >= 51:
+            return "Medium-Minus"
+        elif self.nose_intensity >= 0:
+            return "Low"
+        else:
+            return "Error outputting nose_intensity"
+    
+    def get_development(self):
+        if self.development >= 170:
+            return "Mature"
+        elif self.development >= 85:
+            return "Developing"
+        elif self.development >= 0:
+            return "Youthful" 
+        else:
+            return "Error outputting get_development"
+
+    def get_petillance(self):
+        if self.petillance >= 94:
+            return "Sparkling"
+        elif self.petillance >= 39:
+            return "Semi-Sparkling"
+        elif self.petillance >= 20:
+            return "Spritzy"
+        elif self.petillance >+ 0:
+            return "None"
+        else:
+            return "Error outputting get_petillance"
+
+    def get_sweetness(self):
+        if self.sweetness >= 115:
+            return "Very Sweet"
+        elif self.sweetness >= 45:
+            return "Sweet"
+        elif self.sweetness >= 12:
+            return "Medium-Sweet"
+        elif self.sweetness >= 5:
+            return "Medium-Dry"
+        elif self.sweetness >= 0:
+            return "Dry"
+        else:
+            return "Error outputting get_sweetness"
+
+    def get_acidity(self):
+        if self.acidity >= 204:
+            return "High"
+        elif self.acidity >= 153:
+            return "Medium-Plus"
+        elif self.acidity >= 102:
+            return "Medium"
+        elif self.acidity >= 51:
+            return "Medium-Minus"
+        elif self.acidity >= 0:
+            return "Low"
+        else:
+            return "Error outputting get_acidity"
+
+    def get_alcohol(self):
+        return f"{self.alcohol // 10}-{self.alcohol // 10 + 0.9}%"
+
+    def get_body(self):
+        if self.body >= 204:
+            return "Full"
+        elif self.body >= 153:
+            return "Medium-Plus"
+        elif self.body >= 102:
+            return "Medium"
+        elif self.body >= 51:
+            return "Medium-Minus"
+        elif self.body >= 0:
+            return "Light"
+        else:
+            return "Error outputting get_body"
+
+    def get_tannin_or_bitterness(self):
+        if self.tannin_or_bitterness >= 204:
+            return "High"
+        elif self.tannin_or_bitterness >= 153:
+            return "Medium-Plus"
+        elif self.tannin_or_bitterness >= 102:
+            return "Medium"
+        elif self.tannin_or_bitterness >= 51:
+            return "Medium-Minus"
+        elif self.tannin_or_bitterness >= 30:
+            return "Low"
+        elif self.tannin_or_bitterness >= 0:
+            return "None"
+        else:
+            return "Error outputting get_tannin_or_bitterness"
+
+    def get_finish(self):
+        if self.finish >= 204:
+            return "Long"
+        elif self.finish >= 153:
+            return "Medium-Plus"
+        elif self.finish >= 102:
+            return "Medium"
+        elif self.finish >= 51:
+            return "Medium-Minus"
+        elif self.finish >= 0:
+            return "Short"
+        else:
+            return "Error outputting get_finish"
+    
+    def get_fruit_color(self):
+        if self.fruit_color >= 224:
+            return "Blue"
+        elif self.fruit_color >= 196:
+            return "Black"
+        elif self.fruit_color >= 168:
+            return "Purple"
+        elif self.fruit_color >= 140:
+            return "Dark-Red"
+        elif self.fruit_color >= 112:
+            return "Red"
+        elif self.fruit_color >= 84:
+            return "Orange"
+        elif self.fruit_color >= 56:
+            return "Yellow"
+        elif self.fruit_color >= 28:
+            return "Green"
+        elif self.fruit_color >= 0:
+            return "Very-Green"
+        else:
+            return "Error outputting get_fruit_color"
+
+    def get_fruit_family(self):
+        if self.label_color == "white":
+            if self.fruit_family >= 224:
+                return "Melon"
+            elif self.fruit_family >= 196:
+                return "Sweet Tropical"
+            elif self.fruit_family >= 168:
+                return "Sweet stone"
+            elif self.fruit_family >= 140:
+                return "Sweet Pome"
+            elif self.fruit_family >= 112:
+                return "Tart Tropical"
+            elif self.fruit_family >= 84:
+                return "Tart Stone"
+            elif self.fruit_family >= 56:
+                return "Sweet Citrus"
+            elif self.fruit_family >= 28:
+                return "Tart Pome"
+            elif self.fruit_family >= 0:
+                return "Tart Citrus"
+            else:
+                return "White fruit error"
+        elif self.label_color == "red":
+            if self.fruit_family >= 192:
+                return "Rich Fleshy"
+            elif self.fruit_family >= 128:
+                return "Sweet Stone"
+            elif self.fruit_family >= 64:
+                return "Tart Berry"
+            elif self.fruit_family >= 0:
+                return "Vegetal"
+            else:
+                return "Red fruit error"
+        else:
+            return "Fruit family error. (Only white/red are implemented yet)"
+
+    def get_fruit_condition(self):
+        if self.fruit_condition >= 204:
+            return "Jammy"
+        elif self.fruit_condition >= 153:
+            return "Overripe"
+        elif self.fruit_condition >= 102:
+            return "Ripe"
+        elif self.fruit_condition >= 51:
+            return "Tart"
+        elif self.fruit_condition >= 0:
+            return "Unripe"
+        else:
+            return "Error outputting get_fruit_condition"
+
+    def get_fruit_subcondition(self):
+        if self.fruit_subcondition >= 217:
+            return "Baked"
+        elif self.fruit_subcondition >= 178:
+            return "Cooked"
+        elif self.fruit_subcondition >= 139:
+            return "Dried"
+        elif self.fruit_subcondition >= 100:
+            return "Candied"
+        elif self.fruit_subcondition >= 0:
+            return "None"
+        else:
+            return "Error outputting get_fruit_subcondition"
+
+    def get_floral(self):
+        if self.label_color == "white":
+            if self.floral >= 238:
+                return "Soap"
+            elif self.floral >= 210:
+                return "Perfume"
+            elif self.floral >= 182:
+                return "Geranium"
+            elif self.floral >= 154:
+                return "Rose"
+            elif self.floral >= 126:
+                return "Jasmine"
+            elif self.floral >= 98:
+                return "Honeysuckle"
+            elif self.floral >= 70:
+                return "Faint White Flowers"
+            elif self.floral >= 0:
+                return "None"
+            else:
+                return "White floral error"
+        elif self.label_color == "red":
+            if self.floral >= 225:
+                return "Soap"
+            elif self.floral >= 194:
+                return "Perfume"
+            elif self.floral >= 163:
+                return "Lilac"
+            elif self.floral >= 132:
+                return "Rose"
+            elif self.floral >= 101:
+                return "Violets"
+            elif self.floral >= 70:
+                return "Faint Purple Flowers"
+            elif self.floral >= 0:
+                return "None"
+            else:
+                return "Red floral error"
+        else:
+            return "Floral error. (Only white/red are implemented yet)"
+
+    def get_herbaceous(self):
+        if self.herbaceous >= 212:
+            return "Green Bell Pepper"
+        elif self.herbaceous >= 168:
+            return "Grass"
+        elif self.herbaceous >= 124:
+            return "Asparagus"
+        elif self.herbaceous >= 80:
+            return "Faint Greenness"
+        elif self.herbaceous >= 0:
+            return "None"
+        else:
+            return "Error outputting get_herbaceous"
+
+    def get_herbal(self):
+        if self.herbal >= 210:
+            return "Medicinal Herbs"
+        elif self.herbal >= 165:
+            return "Resinous Herbs"
+        elif self.herbal >= 120:
+            return "Faint Dried Herbs"
+        elif self.herbal >= 0:
+            return "None"
+        else:
+            return "Error outputting get_herbal"
+
+    def get_earth_organic(self):
+        if self.earth_organic >= 216:
+            return "Compost"
+        elif self.earth_organic >= 174:
+            return "Forest Floor"
+        elif self.earth_organic >= 132:
+            return "Potting Soil"
+        elif self.earth_organic >= 90:
+            return "White Mushroom"
+        elif self.get_earth_organic >= 0:
+            return "None"
+        else:
+            return "Error outputting get_earth_organic"
+
+    def get_earth_inorganic(self):
+        if self.earth_inorganic >= 230:
+            return "Scraped Steel"
+        elif self.earth_inorganic >= 202:
+            return "Flint"
+        elif self.earth_inorganic >= 174:
+            return "Chalk"
+        elif self.earth_inorganic >= 146:
+            return "Slate"
+        elif self.earth_inorganic >= 118:
+            return "Wet Pavement"
+        elif self.earth_inorganic >= 90:
+            return "Wet Stone"
+        elif self.get_earth_inorganic >= 0:
+            return "None"
+        else:
+            "Error outputting get_earth_inorganic"
+
+    def get_grape_spice(self):
+        if self.grape_spice >= 226:
+            return "White Pepper"
+        elif self.grape_spice >= 198:
+            return "Black Pepper"
+        elif self.grape_spice >= 169:
+            return "Black Licorice"
+        elif self.grape_spice >= 140:
+            return "Fennel"
+        elif self.grape_spice >= 0:
+            return "None"
+        else:
+            return "Error outputting get_grape_spice"
+
+    def get_oak(self):
+        if self.oak >= 230:
+            return "Coffee"
+        elif self.oak >= 205:
+            return "Burnt Marshmallow"
+        elif self.oak >= 180:
+            return "Vanilla"
+        elif self.oak >= 155:
+            return "Oak Resin"
+        elif self.oak >= 130:
+            return "Sawdust"
+        elif self.oak >= 105:
+            return "Baking Spice"
+        elif self.oak >= 0:
+            return "None"
+        else:
+            return "Error outputting get_oak"
+
+
 class TastingNote:
-    def __init__(self, description, image):
-        self.description = description
-        self.image = image
+
+    def __init__(self, wine):
+        self.wine = wine
+
+    def generate_description(self):
+        output = []
+        output.append(wine.get_clarity())
+        output.append(wine.get_appearance())
+        output_string = ' '.join(output)
+        return output_string
+
+    def __repr__(self):
+        return self.generate_description()
 
 def filter_by_scope(catalog, scope):
     return (wine for wine in catalog if wine.scope <= scope)
@@ -177,14 +534,22 @@ try:
         sql_script = sql_file.read()
 
     conn = sqlite3.connect("catalog.db")
-    conn.row_factory = sqlite3.Row      # necessary to set this so row_factory will work
+    conn.row_factory = sqlite3.Row      # necessary to set this so row_factory will work as desired
     c = conn.cursor()
     c.execute('SELECT * FROM wines')
     rows = c.fetchall()
 
+    # now let's convert each of these into an object
     for row in rows:
         row_dict = dict(row)
-        print(row_dict)
+        wine = Wine(**row_dict)
+        output.append(wine)
+        test_note = TastingNote(wine)
+        print(test_note)
+    
+    # for obj in output:
+    #     print(obj)
+    
 
 finally:
     conn.close()
